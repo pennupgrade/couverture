3003cd8e7b08fa489bd9127ea489994a537e89f9
Charles Wang <czw@seas.upenn.edu>
2024-08-22 23:57:46 -0400

    feat(Healthbar.cs): get max health from tank at Awake

--- Tanks/Assets/Scripts/UI/Healthbar.cs
+++ Tanks/Assets/Scripts/UI/Healthbar.cs
@@ -3,13 +3,14 @@ using UnityEngine.UI;
 
 public class Healthbar : MonoBehaviour
 {
-    [SerializeField] private float maxHealth;
     [SerializeField] private Tank tank;
-
+    
+    private float maxHealth;
     private Slider slider;
 
     private void Awake() {
         slider = gameObject.GetComponent<Slider>();
+        maxHealth = tank.health;
     }
 
     // Update is called once per frame
