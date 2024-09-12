3644458c9455ffc172c4bdfc8bf684cee7b22307
virajdoshi02 <47150131+virajdoshi02@users.noreply.github.com>
2024-09-11 23:07:08 -0400

    Push from Anthony

---
+++ Tanks/Assets/ParticleScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleScript : MonoBehaviour
+{
+    ParticleSystem particle;
+    Vector3 initRot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        particle = GetComponent<ParticleSystem>();
+        initRot = transform.rotation.eulerAngles;
+        Debug.Log(initRot);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ParticleSystem.MainModule test = particle.main;
+        //test.startRotationX = (((transform.rotation.eulerAngles[0] - initRot[0])*Mathf.PI/180));
+        test.startRotationZ = (((transform.rotation.eulerAngles[1] - initRot[1]) * Mathf.PI / 180));
+        //test.startRotationY = (((transform.rotation.eulerAngles[2] - initRot[2]) * Mathf.PI / 180));
+
+    }
+}
