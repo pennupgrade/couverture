8d6cc93648e7c5f406a957f40be10d78413d430d
Anthony Ge <geant@seas.upenn.edu>
2025-01-27 20:42:13 -0500

    Feedback

--- Tanks/Assets/Scripts/EnemyRelated/Enemies/CrabBucket/CrabBucket.cs
+++ Tanks/Assets/Scripts/EnemyRelated/Enemies/CrabBucket/CrabBucket.cs
@@ -8,7 +8,7 @@ public class CrabBucket : MonoBehaviour
     private SphereCollider m_DealDamageCollider;
 
     [SerializeField]
-    float SensingRange = 3;
+    float SensingRange = 3; // I think this variable isn't used - Anthony
     [SerializeField]
     float AttackRange = 2;
     bool attacking = false;
@@ -39,13 +39,14 @@ public class CrabBucket : MonoBehaviour
     void FixedUpdate()
     {
         //For Serialize Field to Work During RunTime
-        m_Collider.radius = SensingRange;
-        m_DealDamageCollider.radius = AttackRange;
+        m_Collider.radius = SensingRange; 
+        m_DealDamageCollider.radius = AttackRange; // is this line and above necessary if you assign their values on 29 and 31 - Anthony
 
         if (attacking)
         {
-            if(attackWaitTimeCounter >= attackWaitTime)
+            if(attackWaitTimeCounter >= attackWaitTime) 
             {
+                // Should we have a delay before the swipe so the player can get ready, or some visual indicator - Anthony
                 Attack();
                 attackWaitTimeCounter = 0;
             }
@@ -53,6 +54,7 @@ public class CrabBucket : MonoBehaviour
             {
                 attackWaitTimeCounter += Time.fixedDeltaTime;
             }
+            // what happens to attackWaitTimeCounter if you aren't attacking, do you want it to go down - Anthony
         }
     }
 
@@ -69,7 +71,9 @@ public class CrabBucket : MonoBehaviour
         if (other.gameObject.tag == "Player")
         {
             attacking = false;
-            attackWaitTimeCounter = attackWaitTime;
+            attackWaitTimeCounter = attackWaitTime; 
+            // if you go in and out of the trigger, the crab will immediately attack
+            // I fee like attackWaitTimeCounter should start at 0 - Anthony
         }
     }
 
@@ -77,7 +81,9 @@ public class CrabBucket : MonoBehaviour
     {
         Debug.Log("One Attack");
         attackEffects.Play();
-        if (attackScript.isPlayerInRange())
+        // Considering the collider is a sphere, you may be better off doing Vector3.Distance or smth
+        // but honestly your choice, I think this is fine - Anthony
+        if (attackScript.isPlayerInRange()) 
         {
             Debug.Log("Deals Damage");
             attackScript.getPlayer().gameObject.GetComponent<Tank>().takeDamage(damage);
