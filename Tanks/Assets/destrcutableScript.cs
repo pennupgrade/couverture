154370c1469503a08f32fb397b6c9a5842da2eb6
Anthony Ge <geant@seas.upenn.edu>
2025-01-27 21:12:40 -0500

    feedback

--- Tanks/Assets/destrcutableScript.cs
+++ Tanks/Assets/destrcutableScript.cs
@@ -7,7 +7,8 @@ public class destrcutableScript : MonoBehaviour
     public Material red;
     public Material green;
     private DamageFlash damageFlash;
-    public GameObject Body;
+    public GameObject Body; // IK i told you that you can just put this.gameObject into DamageFlash, but decide if you're gonna keep this cause rn it's unused - Anthony
+    // maybe add a counter in inspector to denote # of bullets the obj can take before it breaks - Anthony
 
     // Start is called before the first frame update
     void Start()
@@ -29,16 +30,17 @@ public class destrcutableScript : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         Bullet_Default bullet = collision.gameObject.GetComponent<Bullet_Default>();
+
         if(bullet != null)
         {
-            
-            GameObject parent = bullet.parent;
+            // what if parent don't exist, or like say parent is an enemy tank but you kill that tank - Anthony
+            // just make sure you cover that if you still want the below functionalities in the if branches
+            GameObject parent = bullet.parent; 
             if(parent.tag == "Player")
             {
                 Debug.Log("here");
                 damageFlash.CallDamageFlash(this);
                 destroy();
-                
             }
             else if (parent.tag == "Tank")
             {
