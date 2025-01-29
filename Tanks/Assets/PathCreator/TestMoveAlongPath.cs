996afdf8afc344990ccb474b6809473c3ca488aa
Joseph Jeon <josephjeon1990@gmail.com>
2025-01-29 14:22:07 -0500

    moving platforms implemented

---
+++ Tanks/Assets/PathCreator/TestMoveAlongPath.cs
@@ -0,0 +1,51 @@
+using PathCreation;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMoveAlongPath : MonoBehaviour
+{
+    public VertexPath vertexPath;
+    public float speed = 4f;
+
+    public List<Transform> passengerList = new();
+
+    float time = 0f;
+
+    // Update is called once per frame
+    void Update() 
+    {
+        Vector3 oldPos = transform.position;
+        transform.position = vertexPath.MoveConstantVelocity(speed, advanceForward: true);
+        for (int i = 0; i < passengerList.Count; i++)
+        {
+            passengerList[i].position += transform.position - oldPos;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (passengerList.Contains(other.transform))
+        {
+            return;
+        }
+        if (other.gameObject.TryGetComponent(out Tank t)) 
+        {
+            passengerList.Add(t.transform);
+        }
+        if (other.gameObject.TryGetComponent(out Enemy e)) 
+        {
+            passengerList.Add(e.transform);
+        }
+    }
+
+
+    void OnTriggerExit(Collider other)
+    {
+        if (!passengerList.Contains(other.transform))
+        {
+            return;
+        }
+        passengerList.Remove(other.transform);
+    }
+}
