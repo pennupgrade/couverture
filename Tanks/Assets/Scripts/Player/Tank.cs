0331f94311e1b08d95cf864c978bd67173f5d7f1
Saahil Gupta <55856655+seabiscuit-iv@users.noreply.github.com>
2025-01-29 20:36:03 -0500

    TAMP

--- Tanks/Assets/Scripts/Player/Tank.cs
+++ Tanks/Assets/Scripts/Player/Tank.cs
@@ -72,7 +72,7 @@ public class Tank : MonoBehaviour, IDestroyable
 
 
         //Temporary TimedEffect
-        TimedEffect effect = new(5.0f, 
+        TimedEffect effect = new(15.0f, 
             (tank) => {
                 tank.moveSpeed *= 5.0f;
             },
