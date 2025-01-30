dc107cf11403e2f6e47de5bfd9894fc44a2f14fd
Forest Ho-Chen <foresth168@gmail.com>
2025-01-29 22:02:14 -0500

    Stuff

--- Tanks/Assets/Scripts/Player/Tank.cs
+++ Tanks/Assets/Scripts/Player/Tank.cs
@@ -73,16 +73,16 @@ public class Tank : MonoBehaviour, IDestroyable
 
 
         //Temporary TimedEffect
-        TimedEffect effect = new(15.0f, 
-            (tank) => {
-                tank.moveSpeed *= 5.0f;
-            },
-            (tank) => {
-                tank.moveSpeed /= 5.0f;
-            }
-        );
+        // TimedEffect effect = new(15.0f, 
+        //     (tank) => {
+        //         tank.moveSpeed *= 5.0f;
+        //     },
+        //     (tank) => {
+        //         tank.moveSpeed /= 5.0f;
+        //     }
+        // );
 
-        addEffect(effect);
+        // addEffect(effect);
     }
 
 
