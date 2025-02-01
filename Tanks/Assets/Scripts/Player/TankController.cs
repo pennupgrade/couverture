017ff31031e4f86030d4987baaec9c86f9b7165b
Anthony Ge <geant@seas.upenn.edu>
2025-02-01 14:34:24 -0500

    revert to the broken state

--- Tanks/Assets/Scripts/Player/TankController.cs
+++ Tanks/Assets/Scripts/Player/TankController.cs
@@ -58,7 +58,7 @@ public class TankController
 
         Physics.Raycast(origin, direction, out hit, tank.wheelMaxDist);
 
-        if (hit.collider != null && hit.collider.gameObject.layer != 7)
+        if (hit.collider != null)
         {
             wheel.hit = hit;
             wheel.isHit = true;
