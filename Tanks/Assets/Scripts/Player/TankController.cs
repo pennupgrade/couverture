d4e9b1335ff5207de07015558512271cc75a96b9
Anthony Ge <geant@seas.upenn.edu>
2025-02-01 14:13:33 -0500

    small fix

--- Tanks/Assets/Scripts/Player/TankController.cs
+++ Tanks/Assets/Scripts/Player/TankController.cs
@@ -58,7 +58,7 @@ public class TankController
 
         Physics.Raycast(origin, direction, out hit, tank.wheelMaxDist);
 
-        if (hit.collider != null && hit.transform.tag == "Untagged")
+        if (hit.collider != null && hit.transform.tag != "Projectile")
         {
             wheel.hit = hit;
             wheel.isHit = true;
