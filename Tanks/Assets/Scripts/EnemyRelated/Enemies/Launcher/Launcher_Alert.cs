48a1d143f84c35d67715f08b8662a6400c018304
KevinDu10 <kevinwd2401@gmail.com>
2025-02-05 13:57:50 -0500

    Added launcher enemy

---
+++ Tanks/Assets/Scripts/EnemyRelated/Enemies/Launcher/Launcher_Alert.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launcher_Alert : EnemyAlertState
+{
+    private bool playerGone;
+    public Launcher_Alert(Enemy enemy) : base(enemy) {
+    }
+
+    public override Enemy_State Patrol(Vector3 playerPos)
+    {
+        if (enemy.alertPatrol == null) {
+            playerGone = false;
+            enemy.alertPatrol = enemy.StartCoroutine(alertPatroller());
+        } else {
+            if (playerGone) {
+                if (enemy.alertPatrol != null) {
+                    enemy.StopCoroutine(enemy.alertPatrol);
+                    enemy.alertPatrol = null;
+                }  if (enemy.activeShootPeriodically != null) {
+                    enemy.StopCoroutine(enemy.activeShootPeriodically);
+                    enemy.activeShootPeriodically = null;
+                }
+                return new Launcher_Idle(enemy);
+            }
+        }
+        return this;
+    }
+    private IEnumerator alertPatroller() {
+        while (true) {
+            playerGone = getDist() > 12;
+            yield return new WaitForSeconds(6);
+        }
+    }
+
+    public override Enemy_State RotateTurret(Vector3 playerPos) {
+        return this;
+    }
+    public override Enemy_State Shoot(Vector3 playerPos) {
+        if (enemy.activeShootPeriodically == null) {
+            enemy.activeShootPeriodically = enemy.StartCoroutine(shootCor());
+        }
+        return this;
+    }
+    private IEnumerator shootCor() {
+        yield return new WaitForSeconds(0.16f);
+        while (true) {            
+            fire(5);
+            yield return new WaitForSeconds(enemy.reload);
+        }
+    }
+}
