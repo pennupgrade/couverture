4701ae9a9cf16a52f0f52b5d13a200786cb58723
MaEnqiMark <96389187+MaEnqiMark@users.noreply.github.com>
2025-02-07 23:55:21 -0500

    Added 5 Effects on Breakable Objects
    Speed Boost, Damage Boost, Reload Speed Reduction, Add Health, and None. All changes are in "destrcutableScript" (bruh its misspelled are we gonna change it?)

--- Tanks/Assets/destrcutableScript.cs
+++ Tanks/Assets/destrcutableScript.cs
@@ -1,6 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+public enum EffectsOnDestroy
+{
+    SpeedBoost,
+    DamageBoost,
+    ReloadSpeed,
+    AddHealth,
+    None
+};
 
 public class destrcutableScript : MonoBehaviour
 {
@@ -9,6 +19,20 @@ public class destrcutableScript : MonoBehaviour
     private DamageFlash damageFlash;
     public GameObject Body; // IK i told you that you can just put this.gameObject into DamageFlash, but decide if you're gonna keep this cause rn it's unused - Anthony
     // maybe add a counter in inspector to denote # of bullets the obj can take before it breaks - Anthony
+    [SerializeField]
+    public EffectsOnDestroy effectsOnDestroy = EffectsOnDestroy.None;
+    [SerializeField]
+    float speedBoostCoolDown = 5;
+    [SerializeField]
+    float speedBoostMagnitude = 5;
+    [SerializeField]
+    float damageBoostCoolDown = 5;
+    [SerializeField]
+    float damageBoostMagnitude = 5;
+    [SerializeField]
+    float reloadSpeedCoolDown = 5;
+    [SerializeField]
+    float reloadSpeedScale = 0.5f;
 
     // Start is called before the first frame update
     void Start()
@@ -16,15 +40,65 @@ public class destrcutableScript : MonoBehaviour
         damageFlash = new DamageFlash(this.gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
+    void destroy(GameObject parent)
     {
-        
+        Debug.Log("Destroy");
+        addEffectOnDestruction(parent);
+        Destroy(this.gameObject, 0.25f);
     }
 
-    void destroy()
+    private void addEffectOnDestruction(GameObject parent)
     {
-        Destroy(this.gameObject, 0.25f);
+        TimedEffect effectActions = null;
+        switch (effectsOnDestroy)
+        {
+            case EffectsOnDestroy.SpeedBoost:
+                effectActions =
+                    new TimedEffect(speedBoostCoolDown,
+                    (tank) => { tank.moveSpeed += speedBoostMagnitude;
+                        Debug.Log("Speed Boost Start");
+                    },
+                    (tank) => { tank.moveSpeed -= speedBoostMagnitude;
+                        Debug.Log("Speed Boost End");
+                    }  );
+                break;
+            case EffectsOnDestroy.DamageBoost:
+                effectActions =
+                    new TimedEffect(damageBoostCoolDown,
+                    (tank) => { Debug.Log("Damage Boost Start"); },
+                    (tank) => { Debug.Log("Damage Boost End"); }); //don't have access to damage yet 
+                break;
+            case EffectsOnDestroy.ReloadSpeed:
+                effectActions =
+                                   new TimedEffect(reloadSpeedCoolDown,
+                                   (tank) => { Tank.RELOAD_TIME *= reloadSpeedScale;
+                                       Debug.Log("Reload Speed Start");
+                                   },
+                                   (tank) => { Tank.RELOAD_TIME /= reloadSpeedScale;
+                                       Debug.Log("Reload Speed End");
+                                   });
+                break;
+            case EffectsOnDestroy.AddHealth:
+                effectActions =
+                                   new TimedEffect(1,
+                                   (tank) => { tank.health += 100;
+                                       Debug.Log("Add Health");
+                                   }, //not sure if we're doing it like this?
+                                   (tank) => {
+                                       Debug.Log("Health End (No Effect)");
+                                   }); // not deducting the added health back
+                break;
+            case EffectsOnDestroy.None:
+                break;
+            default:
+                break;
+        }
+        if(effectsOnDestroy != EffectsOnDestroy.None && effectActions != null)
+        {
+            Tank player = parent.GetComponent<Tank>();
+            if (player != null)
+                player.addEffect(effectActions);
+        }
     }
     
     private void OnCollisionEnter(Collision collision)
@@ -40,7 +114,7 @@ public class destrcutableScript : MonoBehaviour
             {
                 Debug.Log("here");
                 damageFlash.CallDamageFlash(this);
-                destroy();
+                destroy(parent);
             }
             else if (parent.tag == "Tank")
             {
