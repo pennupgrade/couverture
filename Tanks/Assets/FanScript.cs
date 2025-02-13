9431f720faed49f9bc5066ea94b78fe93a071b34
justclaner <justclaner@gmail.com>
2025-02-12 22:28:45 -0500

    added visual wind to fans

--- Tanks/Assets/FanScript.cs
+++ Tanks/Assets/FanScript.cs
@@ -8,15 +8,18 @@ public class FanScript : MonoBehaviour
     private float exponentialPushPower;  //base in the exponential decay
 
 
+
     private float maxEffectiveDistance;
     public GameObject fanCollider;
+    public ParticleSystem effect;
 
     // Start is called before the first frame update
     void Start()
     {
-        basePushPower = 2.5f;
-        exponentialPushPower = 2f;
+        basePushPower = 0.65f;
+        exponentialPushPower = 4f;
         maxEffectiveDistance = fanCollider.transform.lossyScale.y;
+        effect.startLifetime = fanCollider.transform.lossyScale.y / 6 * 1.3f;
     }
 
     // Update is called once per frame
@@ -28,13 +31,17 @@ public class FanScript : MonoBehaviour
     public void pushPlayer(Collider player)
     {
         float distance = getDistanceFromPlayer(player);
-       
-        player.transform.position +=
-
-            //this factor is always at least 1
-            Mathf.Pow(exponentialPushPower, maxEffectiveDistance - distance)
-            * basePushPower * Time.deltaTime
+        Vector3 displacement = //this factor is always at least 1
+            basePushPower * Time.deltaTime
             * transform.forward;
+
+        float threshold = maxEffectiveDistance / 2.5f;
+        displacement *= (distance >= threshold)
+            ? Mathf.Pow(exponentialPushPower, (maxEffectiveDistance - distance) / maxEffectiveDistance)
+            : Mathf.Pow(maxEffectiveDistance,1.5f);
+
+        player.transform.position += displacement;
+        Debug.Log(distance);
     }
     
     float getDistanceFromPlayer(Collider player)
