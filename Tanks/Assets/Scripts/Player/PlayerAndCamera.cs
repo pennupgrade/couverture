b571e4500891ad8721d72b81da42f8d68a2d775a
exaCORE42 <40a15b8f-bd19-4c61-844c-45c0c4aab94f@aleeas.com>
2025-03-06 12:33:01 -0500

    remove unnecessary Start and Update methods from PlayerAndCamera.cs

--- Tanks/Assets/Scripts/Player/PlayerAndCamera.cs
+++ Tanks/Assets/Scripts/Player/PlayerAndCamera.cs
@@ -23,16 +23,4 @@ public class PlayerAndCamera : MonoBehaviour
             Destroy(gameObject);
         }
     }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
