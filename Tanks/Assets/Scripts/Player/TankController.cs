02075db739f2fd6ccd8ed5cffb0938455841a168
Anthony Ge <geant@seas.upenn.edu>
2025-03-08 00:22:00 -0500

    sublevel 3 adjustments

--- Tanks/Assets/Scripts/Player/TankController.cs
+++ Tanks/Assets/Scripts/Player/TankController.cs
@@ -144,9 +144,12 @@ public class TankController
 
         Vector3 axis = Vector3.Cross(Vector3.up, hitNormal);
 
-        Quaternion targetRotation = axis == Vector3.zero
-            ? tank.Body.transform.rotation 
-            : Quaternion.LookRotation(Vector3.Normalize(axis), hitNormal);
+        if (axis == Vector3.zero)
+        {
+            axis = tank.Body.transform.forward;
+        }
+
+        Quaternion targetRotation = Quaternion.LookRotation(Vector3.Normalize(axis), hitNormal);
 
         tank.Body.transform.rotation = Quaternion.Slerp(tank.Body.transform.rotation, targetRotation, Time.deltaTime * 20.0f);
 
