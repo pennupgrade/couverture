7e4e0a466a4a20a1250457c88e002068bb3b64ec
justclaner <justclaner@gmail.com>
2025-03-12 14:48:30 -0400

    fixed quaternion rotation issue for missile

--- Tanks/Assets/MissileScript.cs
+++ Tanks/Assets/MissileScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MissileScript : MonoBehaviour
 {
-    private float g = -20f;
+    private float g = -9.81f;
 
     public int damage;
     public Vector3 startLocation;
@@ -17,6 +17,7 @@ public class MissileScript : MonoBehaviour
     private bool hitTarget;
     private Vector3 acceleration;
     private Vector3 velocity;
+    private float rotationTwirlInDegrees = 15f;
 
     // Start is called before the first frame update
     void Start()
@@ -37,6 +38,8 @@ public class MissileScript : MonoBehaviour
         float initialVelocityY = (float) ((displacementY - 0.5 * g * Mathf.Pow(secondsInAir,2)) / secondsInAir);
         velocity = new Vector3(initialVelocityX, initialVelocityY, initialVelocityZ);
         Debug.Log(velocity);
+        missileBody.transform.rotation = Quaternion.LookRotation(new Vector3(0, 1, 0));
+       // missileBody.transform.localEulerAngles = new Vector3(-90, 0, 0); //initially facing upward
     }
 
     // Update is called once per frame
@@ -47,11 +50,21 @@ public class MissileScript : MonoBehaviour
         {
             missileBody.transform.position += velocity * Time.deltaTime;
             velocity += acceleration * Time.deltaTime;
+
+
+            //float eulerAngleX = -Mathf.Atan(velocity.y / velocity.z) * Mathf.Rad2Deg;
+            //float eulerAngleY = Mathf.Atan(velocity.z / velocity.x) * Mathf.Rad2Deg;
+            //float eulerAngleZ = Mathf.Atan(velocity.y / velocity.x) * Mathf.Rad2Deg;
+            //missileBody.transform.localEulerAngles =
+            //    new Vector3(eulerAngleX, eulerAngleY, eulerAngleZ);
+            missileBody.transform.rotation = Quaternion.LookRotation(velocity);
+            Debug.Log(missileBody.transform.localEulerAngles);
         } else
         {
             velocity = new Vector3();
             missileBody.transform.position =
                 new Vector3(endLocation.x, endLocation.y + 0.01f, endLocation.z);
+           // missileBody.transform.localEulerAngles = new Vector3(90, 0, 0); //facing downward
         }
     }
 
@@ -59,4 +72,5 @@ public class MissileScript : MonoBehaviour
     {
         hitTarget = value;
     }
+
 }
