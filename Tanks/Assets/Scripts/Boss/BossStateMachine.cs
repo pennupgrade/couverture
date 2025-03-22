89f2f834c046b150bd920cb3cdff3ea5b0a18cea
Charlie Wang <charlie.legobrick@gmail.com>
2025-03-22 15:52:34 -0400

    level 6 boss semi fix

--- Tanks/Assets/Scripts/Boss/BossStateMachine.cs
+++ Tanks/Assets/Scripts/Boss/BossStateMachine.cs
@@ -67,7 +67,11 @@ public class BossStateMachine : MonoBehaviour
     private void Update()
     {
         if (Time.time > stunDur + stunStart) {
+            Vector3 newpos = player.transform.position;
             playerTank.enabled = true;
+            player.GetComponent<Rigidbody>().isKinematic = true;
+            player.transform.position = newpos;
+            print(newpos + " " + player.transform.position);
         }
         timer += Time.deltaTime;
         if (currentState == State.Idle)
@@ -245,6 +249,7 @@ public class BossStateMachine : MonoBehaviour
     void OnCollisionEnter(Collision col) {
         Debug.Log("collided " + col.gameObject.tag + currentAttack + chargeHitAlready);
         if (currentAttack == Attack.Charge && col.gameObject.tag == "Player" && !chargeHitAlready) {
+            col.gameObject.GetComponent<Rigidbody>().isKinematic = false;
             Debug.Log("Player hit!");
             Tank tank = col.gameObject.GetComponent<Tank>();
             Vector3 diff = (player.transform.position - this.transform.position).normalized;
