541c6ddd665e2550eb79fa89eab6c110d913f474
Charlie Wang <charlie.legobrick@gmail.com>
2025-03-22 17:19:55 -0400

    Animal Station in Sublevel 6

--- Tanks/Assets/Scripts/Player/Tank.cs
+++ Tanks/Assets/Scripts/Player/Tank.cs
@@ -150,7 +150,6 @@ public class Tank : MonoBehaviour, IDestroyable
 
         previousPos = currentPos;//this gives them a single-tick of delta difference
         currentPos = transform.position;
-        print("PREV CURR" + previousPos + " " + currentPos);
         
         // FOR TESTING PURPOSES, SHOULD BE REMOVED
         if(Input.GetKeyDown(KeyCode.Z)) {
