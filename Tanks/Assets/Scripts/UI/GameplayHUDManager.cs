1db7c86e5e3f2605861d81bed67c1a8a0253efcd
MaEnqiMark <96389187+MaEnqiMark@users.noreply.github.com>
2025-03-22 16:14:57 -0400

    fill

--- Tanks/Assets/Scripts/UI/GameplayHUDManager.cs
+++ Tanks/Assets/Scripts/UI/GameplayHUDManager.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameplayHUDManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public TMP_Text ready_Text;
+    public Image abilityBar_Fill;
+
+    public void startFillAbilityBar(float reloadTime)
     {
-        
+        abilityBar_Fill.fillAmount = 0;
+        ready_Text.text = "Recharging...";
+        StartCoroutine("fillAbilityBar", reloadTime);
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator fillAbilityBar(float reloadTime)
     {
-        
+        float fillConstant = 1 / reloadTime * 0.05f;
+        while (reloadTime > 0)
+        {
+            yield return new WaitForSeconds(0.05f);
+            reloadTime -= 0.05f;
+            abilityBar_Fill.fillAmount += fillConstant;
+        }
+        ready_Text.text = "Ready!";
     }
 }
