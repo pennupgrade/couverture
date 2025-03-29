cef44179407907527f3ae870e561f5991ea4360d
KevinDu10 <kevinwd2401@gmail.com>
2025-03-29 16:49:34 -0400

    Added engine sounds to all enemies

--- Tanks/Assets/Scripts/EnemyRelated/Enemies/Artemis/Artemis_Start.cs
+++ Tanks/Assets/Scripts/EnemyRelated/Enemies/Artemis/Artemis_Start.cs
@@ -4,6 +4,7 @@ public class Artemis_Start : EnemyStartState
 {
     public Artemis_Start(Enemy enemy) : base(enemy) {}
     protected override Enemy_State stateToTransitionTo(bool alert) {
+        enemy.audioManager.Play("Engine");
         return (alert) ? new Artemis_Alert(enemy) : new Artemis_Idle(enemy);
     }
 }
