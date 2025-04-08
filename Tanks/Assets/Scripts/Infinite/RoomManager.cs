5ccfe31b5113755c9bd3e0154cdd33246fcdac53
KevinDu10 <kevinwd2401@gmail.com>
2025-04-07 21:48:59 -0400

    Classic Mode Setup

--- Tanks/Assets/Scripts/Infinite/RoomManager.cs
+++ Tanks/Assets/Scripts/Infinite/RoomManager.cs
@@ -1,18 +1,73 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RoomManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static RoomManager Instance { get; private set;}
+    public static int LevelNum { get; private set;}
+    bool loading;
+
+    //call at start
+    public static void reset() {
+        LevelNum = 1;
+    }
+    //call when exiting
+    public static void destroyInstance() {
+        Destroy(Instance);
+        Instance = null;
+    }
+    void Awake()
     {
-        
+        if (Instance == null) {
+            Instance = this;
+        } else if (Instance != this) {
+            Destroy(this);
+        }
+        DontDestroyOnLoad(this);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void roomTransition() {
+        if (loading) return;
+
+        LevelNum++;
+
+        //shader transition
+        //level transition
+
+        if (LevelNum == 1) {
+            StartCoroutine(LoadAsyncScene("Classic1"));
+        } else if (LevelNum == 2) {
+            StartCoroutine(LoadAsyncScene("Classic2"));
+        } else {
+            if (LevelNum == 3) {
+                //game end screen, displays time taken, leads to main menu
+
+                //temporary
+                playerDeath();
+            } else {
+                //randomized level
+            }
+        }
+    }
+    public void playerDeath() {
+        // redirect to death screen showing level reached, leads to main menu
+
+        //temporary
+        LevelNum = 0;
+        roomTransition();
+    }
+
+    IEnumerator LoadAsyncScene(string sceneName) {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
+        loading = true;
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+        loading = false;
     }
 }
