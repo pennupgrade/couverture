a13e9d3bd06b4ed93a2adaf3b275afdf1557aee6
Saahil Gupta <55856655+seabiscuit-iv@users.noreply.github.com>
2025-04-23 21:40:20 -0400

    charles has commitment issues

--- Tanks/Assets/Scripts/Player/TankStates/TankState.cs
+++ Tanks/Assets/Scripts/Player/TankStates/TankState.cs
@@ -42,16 +42,19 @@ public abstract class TankState
     protected bool spawnInsideWallCheck()
     {
         Collider[] hitColliders = Physics.OverlapSphere(tank.gunShotPos.position, 0.1f, 1 << 8);
-        foreach (var hit in hitColliders) {
-            if (hit.gameObject.TryGetComponent<Enemy>(out Enemy e)) {
+        foreach (var h in hitColliders) {
+            if (h.gameObject.TryGetComponent<Enemy>(out Enemy e)) {
                 e.takeDamage(100);
                 return true;
             }
         }
-        return
-            Physics.Raycast(tank.gunShotPos.position - 0.2f * tank.gunShotPos.forward,
-                            tank.gunShotPos.forward,
+        bool hit = Physics.Raycast(tank.gunShotPos.position - 0.2f * tank.gunShotPos.forward,
+                            tank.gunShotPos.forward, out RaycastHit hitInfo,
                             Vector3.Distance(tank.gameObject.transform.position, tank.gunShotPos.position), 1 << 3);
+
+
+        // return hit;
+        return hit && hitInfo.collider.gameObject.tag != "Destructable";
     }
 
     public virtual TankState HandleShoot(Vector3 offsetVelocity)
