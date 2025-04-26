41d9ead031e018664f7f2ec013790a61688f5a3b
MaEnqiMark <96389187+MaEnqiMark@users.noreply.github.com>
2025-04-26 15:42:33 -0400

    edited credits screen

--- Tanks/Assets/Scripts/UI/CreditsScreen/CreditScreenController.cs
+++ Tanks/Assets/Scripts/UI/CreditsScreen/CreditScreenController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using TMPro;
 using UnityEngine.SceneManagement;
 
@@ -27,7 +28,7 @@ public class CreditsData
     public List<CreditSection> data;
 }
 
-public class CreditScreenController : MonoBehaviour
+public class CreditScreenController : MonoBehaviour, IBeginDragHandler, IEndDragHandler
 {
     public ScrollRect scrollRect;
     public float scrollSpeed;
@@ -36,11 +37,13 @@ public class CreditScreenController : MonoBehaviour
     public GameObject content;
     public TextAsset jsonFile;
     public Button returnBtn;
+    private bool isScrolling;
 
     private bool readyToScroll = false;
 
     private void Awake()
     {
+        isScrolling = false;
         GenerateCredits();
         StartCoroutine(InitAndStartScroll());
         returnBtn.onClick.AddListener(returnToMain);
@@ -54,8 +57,12 @@ public class CreditScreenController : MonoBehaviour
     private void GenerateCredits()
     {
         content.SetActive(true); // Must be active before building
-        GameObject temp1 = Instantiate(prefab_SectionTitle, content.transform);
-        temp1.GetComponent<TMP_Text>().text = "  ";
+
+        for (int i = 0; i < 4; i++)
+        {
+            GameObject temp1 = Instantiate(prefab_SectionTitle, content.transform);
+            temp1.GetComponent<TMP_Text>().text = "  ";
+        }
 
         CreditsData creditsData = JsonUtility.FromJson<CreditsData>(jsonFile.text);
         foreach (CreditSection section in creditsData.data)
@@ -75,6 +82,8 @@ public class CreditScreenController : MonoBehaviour
         }
     }
 
+    
+
     private IEnumerator InitAndStartScroll()
     {
         // Force layout rebuild
@@ -92,9 +101,19 @@ public class CreditScreenController : MonoBehaviour
 
     private void Update()
     {
-        if (readyToScroll && scrollRect.verticalNormalizedPosition > 0)
+        if (readyToScroll && scrollRect.verticalNormalizedPosition > 0 && !isScrolling)
         {
             scrollRect.verticalNormalizedPosition -= scrollSpeed * Time.deltaTime / 100f;
         }
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        isScrolling = true;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        isScrolling = false;
     }
 }
