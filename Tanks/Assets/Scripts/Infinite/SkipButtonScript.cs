77ef34e8aa10bd3d9c1052e6ed8406af7841ff7f
KevinDu10 <kevinwd2401@gmail.com>
2025-04-28 17:01:02 -0400

    Classic Skip Setup

---
+++ Tanks/Assets/Scripts/Infinite/SkipButtonScript.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkipButtonScript : Activatable
+{
+    void Start() {
+        if (false) {
+            gameObject.SetActive(false);
+        }
+    }
+    public override void activate() {
+        
+    }
+}
