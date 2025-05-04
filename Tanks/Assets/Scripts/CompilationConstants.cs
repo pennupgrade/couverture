de4cf6e6fe57439d2f9de288a66dd66c0e0ba2f2
exaCORE42 <40a15b8f-bd19-4c61-844c-45c0c4aab94f@aleeas.com>
2025-05-04 19:13:36 -0400

    Clean up code

--- Tanks/Assets/Scripts/CompilationConstants.cs
+++ Tanks/Assets/Scripts/CompilationConstants.cs
@@ -5,11 +5,11 @@ public class CompilationConstants {
     // game data path
     // Steam game data path 
     # if STEAM
-        public static readonly string GAME_DATA_PATH = Path.GetFullPath(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "UPGRADE/Catanks/");
+        public static readonly string GAME_DATA_PATH = Path.GetFullPath("./UPGRADE/Catanks/", Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData));
     # else
         public static readonly string GAME_DATA_PATH = Environment.CurrentDirectory;
     # endif
 
     // Get absolute path (recommended by Microsoft: https://learn.microsoft.com/en-us/dotnet/standard/io/file-path-formats#path-normalization)
-    public static readonly string SAVE_DATA_PATH = Path.GetFullPath(GAME_DATA_PATH, "SAVE_DATA/");
+    public static readonly string SAVE_DATA_PATH = Path.GetFullPath("./SAVE_DATA/", GAME_DATA_PATH);
 }
