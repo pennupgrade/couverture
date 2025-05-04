7c400926b6653c8e05dbc223d39560ea2ab2821b
exaCORE42 <40a15b8f-bd19-4c61-844c-45c0c4aab94f@aleeas.com>
2025-05-03 22:48:18 -0400

    More progress for different save directories for different versions

--- Tanks/Assets/Scripts/CompilationConstants.cs
+++ Tanks/Assets/Scripts/CompilationConstants.cs
@@ -1,13 +1,20 @@
 public class CompilationConstants {
     // save file path
-    public const string SAVE_DATA_PATH =
+        // Steam save file paths 
         # if STEAM && UNITY_STANDALONE_WIN
-            "";
+            public const string SAVE_DATA_PATH = "";
         # elif STEAM && UNITY_STANDALONE_LINUX
-            "";
+            public const string SAVE_DATA_PATH = "";
         # elif STEAM && UNITY_STANDALONE_OSX
-            ""; // TODO: FINISH
+            public const string SAVE_DATA_PATH = "";
+        // Non-Steam save file paths
+        # elif UNITY_STANDALONE_LINUX || UNITY_STANDALONE_OSX
+            public const string SAVE_DATA_PATH = "SAVE_DATA/";
+        # elif UNITY_STANDALONE_WIN
+            // Windows has a different syntax for path separators
+            public const string SAVE_DATA_PATH = "SAVE_DATA\\";
         # else
-            "SAVE_DATA/";
+            // shouldn't occur
+            public const string SAVE_DATA_PATH = null;
         # endif
 }
