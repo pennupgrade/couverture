da600dcc39d5e6cf265ec09a2e7f0cf47cf18f5d
exaCORE42 <40a15b8f-bd19-4c61-844c-45c0c4aab94f@aleeas.com>
2025-05-13 14:34:26 -0400

    switch to AppDomain.CurrentDomain.BaseDirectory for game data

--- Tanks/Assets/Scripts/CompilationConstants.cs
+++ Tanks/Assets/Scripts/CompilationConstants.cs
@@ -7,7 +7,7 @@ public class CompilationConstants {
     #if STEAM
         public static readonly string GAME_DATA_PATH = Path.GetFullPath("./UPGRADE/Catanks/", Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData));
     #else
-        public static readonly string GAME_DATA_PATH = Environment.CurrentDirectory;
+        public static readonly string GAME_DATA_PATH = AppDomain.CurrentDomain.BaseDirectory;
     #endif
 
     // Get absolute path (recommended by Microsoft: https://learn.microsoft.com/en-us/dotnet/standard/io/file-path-formats#path-normalization)
