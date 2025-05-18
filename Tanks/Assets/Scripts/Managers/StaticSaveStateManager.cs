737da63e053e1073a3d69ff67c471c8bd1b78161
exaCORE42 <40a15b8f-bd19-4c61-844c-45c0c4aab94f@aleeas.com>
2025-05-18 15:19:55 -0400

    remove unnecessary comment, ensure that static save file is saved upon creation

--- Tanks/Assets/Scripts/Managers/StaticSaveStateManager.cs
+++ Tanks/Assets/Scripts/Managers/StaticSaveStateManager.cs
@@ -8,7 +8,7 @@ using UnityEngine;
 public class StaticSaveStateManager : SaveStateManager {
     public static readonly string CLASSIC_MODE_SAVE_PATH = SaveStateManagerGameObject.CLASSIC_MODE_SAVE_FILE;
     public static StaticSaveStateManager LoadStaticSave() {
-        CreateSaveDirectory(); // this shouldn't ever be used, but it's here just for safety
+        CreateSaveDirectory();
         try {
             StaticSaveStateManager outManager;
             using (StreamReader reader = new(CLASSIC_MODE_SAVE_PATH)) {
@@ -21,6 +21,7 @@ public class StaticSaveStateManager : SaveStateManager {
         catch (FileNotFoundException) {
             StaticSaveStateManager outManager = new();
             outManager.CreateNewSave(CLASSIC_MODE_SAVE_PATH);
+            outManager.SaveToFile();
             return outManager;
         }
     }
