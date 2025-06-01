240a5576a1a6d699b82b644d3923095aeea8350f
Charles Wang <zwcharl@gmail.com>
2025-05-31 22:29:24 -0700

    conditionally add achievements btn, add frame img, begin achievements canvas

---
+++ Tanks/Assets/Scripts/UI/TitleScreen/SidePanelBehavior.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SidePanelBehavior : MonoBehaviour
+{
+    [SerializeField] private RectTransform rt;
+    [SerializeField] private GameObject achievementsButton;
+
+    private void Start()
+    {
+        var sizeDeltaX = rt.sizeDelta.x;
+
+#if !(DISABLESTEAMWORKS || UNITY_EDITOR)
+        // Decrease vertical layout group's height because achivements button is missing
+        rt.sizeDelta = new Vector2(sizeDeltaX, 420f);
+        achievementsButton.SetActive(false);
+#else
+        rt.sizeDelta = new Vector2(sizeDeltaX, 530f);
+        achievementsButton.SetActive(true);
+#endif
+    }
+}
