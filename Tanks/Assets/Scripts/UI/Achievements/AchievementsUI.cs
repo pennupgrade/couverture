a49bdfb28cac42f61c032c0b3808fd8bf50a23b6
exaCORE42 <40a15b8f-bd19-4c61-844c-45c0c4aab94f@aleeas.com>
2025-07-17 15:23:04 -0400

    fix achievements UI bug

--- Tanks/Assets/Scripts/UI/Achievements/AchievementsUI.cs
+++ Tanks/Assets/Scripts/UI/Achievements/AchievementsUI.cs
@@ -105,7 +105,7 @@ public class AchievementsUI : MonoBehaviour
         bigMedalRt.localScale = sourceRt.localScale * 0.5f; // Big medal scale is half of everything
         bigMedalRt.localRotation = sourceRt.localRotation;
 
-        description.text = achievement.title;
+        description.text = achievement.description;
 
         var bigMedal = bigMedalRt.gameObject.GetComponent<Medal>();
         bigMedal.SetMedalTexture(achievement.medal);
