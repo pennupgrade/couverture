a0916547158c0140e28236e0433ecee29ffb4e13
exaCORE42 <40a15b8f-bd19-4c61-844c-45c0c4aab94f@aleeas.com>
2025-07-17 15:07:23 -0400

    replace STEAM preprocessor symbol with !DISABLESTEAMWORKS

--- Tanks/Assets/Scripts/CompilationConstants.cs
+++ Tanks/Assets/Scripts/CompilationConstants.cs
@@ -8,7 +8,7 @@ using System.IO;
 public class CompilationConstants {
     // game data path
     // Steam game data path 
-    #if STEAM
+    #if !DISABLESTEAMWORKS
         public static readonly string GAME_DATA_PATH = Path.GetFullPath("./UPGRADE/Catanks/", Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData));
     #elif UNITY_EDITOR // game data path when using unity editor
         public static readonly string GAME_DATA_PATH = Environment.CurrentDirectory;
