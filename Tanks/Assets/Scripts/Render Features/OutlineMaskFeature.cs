c8132390dcc8ef2e920527391c586249bbe493c9
exaCORE42 <40a15b8f-bd19-4c61-844c-45c0c4aab94f@aleeas.com>
2025-07-17 20:17:51 -0400

    add license information to OutlineMaskFeature.cs

--- Tanks/Assets/Scripts/Render Features/OutlineMaskFeature.cs
+++ Tanks/Assets/Scripts/Render Features/OutlineMaskFeature.cs
@@ -1,3 +1,8 @@
+// SPDX-FileCopyrightText: 2023 Logan Cho
+// SPDX-FileCopyrightText: 2024 The Catanks Contributors
+//
+// SPDX-License-Identifier: MPL-2.0
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
